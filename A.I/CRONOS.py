cd2facd3cfb0d243623ae6c2270e91c5a94cf01f
alter  iterskiy <alter99@yahoo.com>
2022-12-07 18:26:54 -0500

    1

--- A.I/CRONOS.py
+++ A.I/CRONOS.py
@@ -1,30 +1,29 @@
-#imports
-from PyQt6.QtWidgets import *
-from PyQt6.QtGui import *
-from PyQt6.QtCore import *
+# importing the required libraries
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 import sys
 
-#window
 class Window(QMainWindow):
-    def __init__(self):
-        super().__init__()
+	def __init__(self):
+		super().__init__()
+		
+		# string value
+		title = "Title of window"
 
-title = "Cronos"
- 
-        # set the title
-self.setWindowTitle(title)
- 
-        # setting  the geometry of window
-self.setGeometry(0, 0, 500, 300)
- 
-        # show all the widgets
-self.show()
- 
+		# set the title
+		self.setWindowTitle(title)
+
+		# setting the geometry of window
+		self.setGeometry(0, 0, 500, 300)
+
+		# show all the widgets
+		self.show()
+
 # create pyqt5 app
 App = QApplication(sys.argv)
- 
+
 # create the instance of our Window
 window = Window()
- 
+
 # start the app
 sys.exit(App.exec())
