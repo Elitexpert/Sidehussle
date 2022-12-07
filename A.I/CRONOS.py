86cc70e02d5fda420fffa4a9d404d038da42a533
alter  iterskiy <alter99@yahoo.com>
2022-12-07 14:43:04 -0500

    added modern window

--- A.I/CRONOS.py
+++ A.I/CRONOS.py
@@ -2,44 +2,17 @@
 from tkinter import *
 import customtkinter
   
-# Create object 
-root = Tk()
+# Create ctk window 
+root = customtkinter.CTk()
   
 # Adjust size 
 root.geometry("400x400")
   
-# Add image file
-bg = PhotoImage(file = "C:\Users\alter\OneDrive\Documents\GitHub\Sidehussle\Sidehussle-1\project0\Images\499419.jpg")
-  
-# Create Canvas
-canvas1 = Canvas( root, width = 400,
-                 height = 400)
-  
-canvas1.pack(fill = "both", expand = True)
-  
-# Display image
-canvas1.create_image( 0, 0, image = bg, 
-                     anchor = "nw")
-  
-# Add Text
-canvas1.create_text( 200, 250, text = "Welcome")
-  
-# Create Buttons
-button1 = Button( root, text = "Exit")
-button3 = Button( root, text = "Start")
-button2 = Button( root, text = "Reset")
-  
-# Display Buttons
-button1_canvas = canvas1.create_window( 100, 10, 
-                                       anchor = "nw",
-                                       window = button1)
-  
-button2_canvas = canvas1.create_window( 100, 40,
-                                       anchor = "nw",
-                                       window = button2)
-  
-button3_canvas = canvas1.create_window( 100, 70, anchor = "nw",
-                                       window = button3)
-  
+#CTK Buttons
+Button=customtkinter.CTkRadioButton(master=root, text="Hi im Cronos . Sidehussles Helper")
+
+#show at center of screen
+Button.place(relx=0.5, rely=0.5, anchor=CENTER)
+
 # Execute tkinter
 root.mainloop()
