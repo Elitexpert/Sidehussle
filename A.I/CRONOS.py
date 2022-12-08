55d3918693cccaef714221fcd6588e11ee231824
Alter <alteriterskiy@outlook.com>
2022-12-07 23:42:01 -0500

    1

--- A.I/CRONOS.py
+++ A.I/CRONOS.py
@@ -4,35 +4,42 @@ import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+from PyQt5 import *
+from time import *
 
 
-class Window(QMainWindow):
+class App(QWidget):
 	def __init__(self):
 		super().__init__()
+		self.Title = 'Cronos'
+		self.left = 10
+		self.top = 10
+		self.width = 640
+		self.height = 480
+		self.initUI()
 
 
-		# set the title
-		self.setWindowTitle("Cronos")
-
-		# setting the geometry of window
-		self.setGeometry(60, 60, 600, 400)
+	def initUI(self):
+		self.setWindowTitle(self.Title)
+		self.setGeometry(self.left, self.top, self.width, self.height)
 
 
-		# creating a window background
+		# creating a window
 		label = QLabel(self)
-		pixmap = QPixmap('C:\Users\alter\OneDrive\Documents\Sidehussle\project0\Images\499419.jpg')
+		pixmap = QPixmap(r'C:\Users\alter\OneDrive\Documents\Sidehussle\project0\Images\499419.jpg')
 		label.setPixmap(pixmap)
-
-
-		# show all the widgets
+		self.resize(pixmap.width(),pixmap.height())
 		self.show()
+
+		#create animations
+		Line_1="Hello im Cronos"
+		Line_2="Im Sidehussle little buddy"
+		Line_3="I heard your looking for a job"
+		line_4=""
 
 
 # create pyqt5 app
-App = QApplication(sys.argv)
-
-# create the instance of our Window
-window = Window()
-# start the app
-sys.exit(App.exec())
-
+if __name__=='__main__':
+	app = QApplication(sys.argv)
+	ex = App()
+	sys.exit(app.exec_())
