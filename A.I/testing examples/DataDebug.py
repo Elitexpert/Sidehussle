f6179958d3d4ffe4e0a7f81a52baf622f420454f
alter  iterskiy <alter99@yahoo.com>
2023-01-06 01:37:51 -0500

    1

---
+++ A.I/testing examples/DataDebug.py
@@ -0,0 +1,161 @@
+import sys
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QApplication, QGridLayout, QLabel, QLineEdit, QPushButton, QTextEdit, QWidget
+)
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # Set up the user interface
+        self.initUI()
+
+    def initUI(self):
+        # Create the input and output fields
+        input_label = QLabel("Input:")
+        self.input_field = QLineEdit()
+        output_label = QLabel("Output:")
+        self.output_field = QTextEdit()
+
+        # Create the "Run" button
+        run_button = QPushButton("Run")
+        run_button.clicked.connect(self.run_code)
+
+        # Add the widgets to a grid layout
+        grid = QGridLayout()
+        grid.addWidget(input_label, 0, 0)
+        grid.addWidget(self.input_field, 0, 1)
+        grid.addWidget(output_label, 1, 0)
+        grid.addWidget(self.output_field, 1, 1)
+        grid.addWidget(run_button, 2, 1, Qt.AlignRight)
+
+        self.setLayout(grid)
+
+        # Set the window properties
+        self.setGeometry(300, 300, 300, 150)
+        self.setWindowTitle("TensorFlow GUI")
+
+    def run_code(self):
+        # Load the data
+        questions = [
+            "Hi",
+            "What is your name?",
+            "How old are you?",
+            "Where are you from?",
+            "What do you do?",
+            "What are your hobbies?",
+        ]
+
+        answers = [
+            "Hello",
+            "My name is Alice.",
+            "I am 25 years old.",
+            "I am from New York.",
+            "I am a software engineer.",
+            "I enjoy reading, hiking, and playing video games.",
+        ]
+
+        # Preprocess the data
+        def load_and_preprocess_data():
+            vocab = set(questions + answers)
+            vocab_size = len(vocab)
+
+            # Create a vocabulary index
+            vocab_index = {word: index for index, word in enumerate(vocab)}
+
+            # Encode the data
+            def encode(sentence):
+                encoded_sentence = []
+                for word in sentence.split():
+                    if word in vocab_index:
+                        encoded_sentence.append(vocab_index[word])
+                return encoded_sentence
+
+            x_train = np.array([encode(question) for question in questions])
+            y_train = np.array([encode(answer) for answer in answers])
+
+            # Create a validation set
+            x_val = x_train[:2]
+            y_val = y_train[:2]
+
+            # Use the remaining data for training
+            x_train = x_train[2:]
+            y_train = y_train[2:]
+            return (x_train,y_train)
+           
+    # Build the model
+inputs = tf.keras.Input(shape=(None,), dtype=tf.int32)
+embeddings = tf.keras.layers.Embedding(vocab_size, embedding_dim)(inputs)
+encoder_outputs, state_h, state_c = tf.keras.layers.LSTM(encoder_units, return_state=True)(embeddings)
+encoder_states = [state_h, state_c]
+
+decoder_inputs = tf.keras.Input(shape=(None,), dtype=tf.int32)
+decoder_embeddings = tf.keras.layers.Embedding(vocab_size, embedding_dim)(decoder_inputs)
+decoder_lstm = tf.keras.layers.LSTM(decoder_units, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_embeddings, initial_state=encoder_states)
+outputs = tf.keras.layers.Dense(vocab_size, activation="softmax")(decoder_outputs)
+
+model = tf.keras.Model([inputs, decoder_inputs], outputs)
+model.compile(optimizer=tf.keras.optimizers.Adam(), loss="sparse_categorical_crossentropy")
+
+# Train the model
+history = model.fit(
+    [x_train, y_train[:, :-1]], y_train[:, 1:],
+    validation_data=([x_val, y_val[:, :-1]], y_val[:, 1:]),
+    epochs=epochs,
+    batch_size=batch_size,
+)
+
+# Plot the training and validation loss
+plt.plot(history.history["loss"], label="Training loss")
+plt.plot(history.history["val_loss"], label="Validation loss")
+plt.legend()
+plt.show()
+
+# Test the model
+def evaluate(sentence):
+    sentence = preprocess_sentence(sentence)
+
+    encoded_sentence = encode(sentence)
+    encoded_sentence = np.expand_dims(encoded_sentence, axis=0)
+
+    output = model.predict([encoded_sentence, np.zeros((1, 1))])
+
+    return output[0]
+
+def decode(encoded_sentence):
+    decoded_sentence = []
+    for word_index in encoded_sentence:
+        if word_index == 0:
+            break
+        decoded_sentence.append(vocab[word_index])
+    return ' '.join(decoded_sentence)
+
+for question in test_questions:
+    encoded_question = encode(preprocess_sentence(question))
+    encoded_question = np.expand_
+
+# Save the model
+model.save("s2s.h5")
+
+# Load the model
+model = tf.keras.models.load_model("s2s.h5")
+
+# Define the inference model
+encoder_model = tf.keras.Model(inputs, encoder_states)
+
+decoder_state_input_h = tf.keras.Input(shape=(decoder_units,))
+decoder_state_input_c = tf.keras.Input(shape=(decoder_units,))
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+decoder_outputs, state_h, state_c = decoder_lstm(decoder_embeddings, initial_state=decoder_states_inputs)
+decoder_states = [state_h, state_c]
+decoder_outputs = outputs(decoder_outputs)
+decoder_model = tf.keras.Model(
+    [decoder_inputs] + decoder_states_inputs,
+    [decoder_outputs] + decoder_states)
+        
